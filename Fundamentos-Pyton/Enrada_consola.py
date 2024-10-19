e0089bcea002c75e96cc03eacc605d615e002d65
Alberto <163355369+A163r70@users.noreply.github.com>
2024-10-19 16:07:53 -0600

    Update Enrada_consola.py

--- Fundamentos-Pyton/Enrada_consola.py
+++ Fundamentos-Pyton/Enrada_consola.py
@@ -47,11 +47,11 @@ promedio = float(input("Ingresa el promedio: "))
 es_mujer = input("¿Es mujer (Si/No)?: ")
 
 # No es posible convertir directamente una cadena a un valor booleano.
-# Por ello, utilizamos la misma variable, convertimos a  minúsculas y lo comparamos con la cadena "si".
+# Por ello, utilizamos la misma variable, convertimos a  minúsculas con la función lower() y lo comparamos con la cadena "si".
 es_mujer = es_mujer.lower() == "si"
 
 # Se imprimen los datos del alumno.
 # Comentar  qué es lo que realiza {promedio:.2f} probando con números diferentes.
+#{promedio:.2f} hace que después del punto solo imprima dos dígitos.
 print()
 print(f"El alumno {nombre} cursa el {semestre} semestre con un promedio de {promedio:.2f}. Además, es mujer: {es_mujer}.")
-
