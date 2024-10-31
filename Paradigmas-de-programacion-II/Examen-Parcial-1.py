c3ca5af72c97106adcb5ec2b5dd1edcab8e81eb4
AlbertoSC2 <albertodesalinas09@.com>
2024-10-31 15:23:29 -0600

    Ejercicios de while con if-elseif

---
+++ Paradigmas-de-programacion-II/Examen-Parcial-1.py
@@ -0,0 +1,12 @@
+'''
+Nombre: Jesús Alberto Ramírez Salinas
+Fecha: 31 de Octubre de 2024
+Descripción: Ejercico 1 del examen parcial.
+'''
+
+print("** Licenciatura en Informática **")
+final = int(input("Ingrese el número final de la cuenta: "))
+Contador = 1
+while Contador <= final:
+    print(Contador, end=" ")
+    Contador += 1
