8f61403362dfdc0dcf580e530c779c0e1d57d4c4
A163r70 <albertodesalinas09@gmail.com>
2024-11-13 21:34:40 -0600

    Ejercicios sobre listas y funciones

--- Funciones_y_Colecciones/Listas_ejercicio3_promedios.py
+++ Funciones_y_Colecciones/Listas_ejercicio3_promedios.py
@@ -4,19 +4,48 @@ Fecha: 11 de noviembre de 2024
 Descripción: Segundo ejercicio sobre listas.
 '''
 
-calificaciones =
+alumnos = [ ]
+esrtuctura_datos = [ ]
+algebra = [ ]
+contabilidad = [ ]
+electronicaII = [ ]
+derecho_legislacion = [ ]
+ingles = [ ]
+materias = esrtuctura_datos+algebra+contabilidad+electronicaII+derecho_legislacion+ingles
 
 def menu():
     print("1.- Ver calificaciones de alumno")
     print("2.- Ver promedio de alumnos")
     print("3.- Añadir alumno")
-    print("0.- Eliminar alumno")
-    print("0.- Ver promedio grupal")
+    print("4.- Eliminar alumno")
+    print("5.- Ver promedio grupal")
     print("0.- Salir")
     opcion = int(input("Elige una opción: "))
     print()
     return opcion
 
-#lista para cada materia
-#cuando se ingresa un alumno se ingresa a la lista de alumnoss, y las calificaciones se agregan a la lista de
-#calificaciones
+def ver_calificaciones(materias):
+    numero_alumno = int(input("Ingrese el número del alumno que desea ver sus calificaciones: "))
+    for i in materias:
+        print(materias[1][numero_alumno])
+    print()
+
+def nuevo_alumno(alumnos):
+    alumno = input("Ingresa al nuevo alumno: ")
+    alumnos.append(alumno)
+
+contador = 1
+contador_alumno = 0
+while contador != 0:
+    opciones = menu()
+    if opciones == 1:
+        ver_calificaciones(materias)
+    elif opciones == 2:
+        nuevo_alumno(alumnos)
+        contador_alumno +=1
+    elif opciones == 3:
+        eliminar(nombre, cantidad, contador_productos)
+    elif opciones == 0:
+        contador = 0
+    else:
+        print("Valor no válido.")
