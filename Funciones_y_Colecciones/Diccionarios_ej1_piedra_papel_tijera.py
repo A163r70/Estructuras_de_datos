b9891f5c979cca82df9639bf93b392696362c6a0
A163r70 <albertodesalinas09@gmail.com>
2024-12-02 22:33:43 -0600

    Ejercicios sobre conjuntos

---
+++ Funciones_y_Colecciones/Diccionarios_ej1_piedra_papel_tijera.py
@@ -0,0 +1,16 @@
+'''
+Nombre: Jesús Alberto Ramírez Salinas
+Fecha: 02 de diciembre de 2024
+Descripción: Primer ejercicio sobre diccionarios.
+'''
+
+
+def menu():
+    print("***  Juego de piedra, papel o tijeras  ***")
+    print("1.- Piedra.")
+    print("2.- Papel.")
+    print("3.- Tijeras.")
+    print("Salir.")
+    opcion = input("Ingresa una opción: ")
+    return opcion
+
