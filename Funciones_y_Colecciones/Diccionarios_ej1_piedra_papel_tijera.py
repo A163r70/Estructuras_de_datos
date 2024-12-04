aa94ec8b82c4edabacf4f9b10f6f31906ca49e76
AlbertoSC2 <albertodesalinas09@.com>
2024-12-04 16:17:54 -0600

    Diccionarios

--- Funciones_y_Colecciones/Diccionarios_ej1_piedra_papel_tijera.py
+++ Funciones_y_Colecciones/Diccionarios_ej1_piedra_papel_tijera.py
@@ -5,6 +5,10 @@ Descripción: Primer ejercicio sobre diccionarios.
 '''
 from random import choice
 
+victorias = 0
+empates = 0
+victorias_cpu = 0
+
 piedra_papel_tijeras = {('PIEDRA', 'TIJERAS'): "JUGADOR",
                         ('PIEDRA', 'PAPEL'): "CPU",
                         ('PAPEL', 'PIEDRA'): "JUGADOR",
@@ -12,12 +16,13 @@ piedra_papel_tijeras = {('PIEDRA', 'TIJERAS'): "JUGADOR",
                         ('TIJERAS', 'PAPEL'): "JUGADOR",
                         ('TIJERAS', 'PIEDRA'): "CPU"}
 
-def menu():
+def menu(victorias, empates, victorias_cpu):
+    print(f"Victorias del jugador: {victorias}, empates: {empates}, victorias del CPU: {victorias_cpu}")
     print("***  Juego de piedra, papel o tijeras  ***")
     print("1.- Piedra.")
     print("2.- Papel.")
     print("3.- Tijeras.")
-    print("Salir.")
+    print("0.- Salir.")
     opcion = input("Ingresa una opción: ")
     return opcion
 
@@ -29,10 +34,28 @@ def jugador(opcion):
         eleccion_jugador = "PAPEL"
     elif opcion == 3:
         eleccion_jugador = "TIJERAS"
+    elif opcion == 0:
+        return
     else:
         print("Valor no valido.")
     eleccion_cpu = choice(["PIEDRA", "PAPEL", "TIJERAS"])
 
     return eleccion_jugador, eleccion_cpu
 
+def juego(eleccion_jugador, eleccion_cpu, piedra_papel_tijeras, victorias, victorias_cpu, empates):
+    resultado = piedra_papel_tijeras.get((eleccion_jugador, eleccion_cpu), "EMPATE")
+    if resultado == "JUGADOR":
+        print(f"Jugador: {eleccion_jugador}. CPU: {eleccion_cpu}. El resultado es: {resultado}")
+        victorias += 1
+    elif resultado == "CPU":
+        print(f"Jugador: {eleccion_jugador}. CPU: {eleccion_cpu}. El resultado es: {resultado}")
+        victorias_cpu += 1
+    else:
+        print(f"Jugador: {eleccion_jugador}. CPU: {eleccion_cpu}. El resultado es: EMPATE")
+        empates += 1
 
+    return victorias, victorias_cpu, empates
+
+menu(victorias, empates, victorias_cpu)
+jugador(menu(victorias, empates, victorias_cpu))
+juego(jugador(menu(victorias, empates, victorias_cpu)), piedra_papel_tijeras, victorias, victorias_cpu, empates)
