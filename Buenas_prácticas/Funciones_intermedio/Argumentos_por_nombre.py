d621c8c71815451523bea342e7ed920bbaad9485
AlbertoSC2 <albertodesalinas09@.com>
2025-01-16 15:45:38 -0600

    main

--- Buenas_prácticas/Funciones_intermedio/Argumentos_por_nombre.py
+++ Buenas_prácticas/Funciones_intermedio/Argumentos_por_nombre.py
@@ -4,7 +4,16 @@ Fecha: 13 de enero de 2025
 Descripción: Conversiones.
 '''
 
-def imprimir_alumno(nombre: str, edad: int, matricula: int, grupo: int, semestre: int)-> None:
+def imprimir_alumno(nombre: str, edad: int, matricula: int, grupo: int, *, semestre: int= 3)-> None:
+    """
+    Función que imprime datos de un alumno.
+    :param nombre: Nombre del alumno.
+    :param edad: Edad del alumno.
+    :param matricula: Matrícula del alumno
+    :param grupo: Grupo del alumno.
+    :param semestre: Semetre del alumno.
+    :return:
+    """
     print("*** Datos personales ***")
     print(f"Nombre: {nombre}")
     print(f"Edad: {edad}")
@@ -13,16 +22,22 @@ def imprimir_alumno(nombre: str, edad: int, matricula: int, grupo: int, semestre
     print(f"Semestre: {semestre}")
 
 
-
 def main()->None:
-    pass
+
+    imprimir_alumno("Alberto", 31, 123, 303)
+
+    # Argumentos por nombre
+    imprimir_alumno(nombre="Jesús", edad=20, matricula=12392394, grupo=0, semestre=3)
+
+    imprimir_alumno("Alberto", 31, 123, grupo= 3, semestre= 2)
 
 if __name__ == '__main__':
     nombre = "Alberto"
     edad = 19
     matricula = 12305839
     grupo = 303
-    semetre = 3
+    #semetre = 3
 
     main()
-    imprimir_alumno(nombre,edad,matricula,grupo,semetre)
+    imprimir_alumno(nombre,edad,matricula, grupo)
+
